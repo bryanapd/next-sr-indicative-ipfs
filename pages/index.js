a5db74bff11b438b18ac0f797bce5a700fb0f103
Bryan Diolata <bryan.appdeture@gmail.com>
2022-04-20 14:59:03 +0800

    Updated IPFS

--- pages/index.js
+++ pages/index.js
@@ -1,153 +1,235 @@
-import { 
-  Box, Text, HStack, Stack, Button, Container, Input,
-  Heading, Image, Center, Link
-} from '@chakra-ui/react'
-import styles from '../styles/Home.module.css'
-import { useState } from "react"
-import { create, urlSource } from 'ipfs-http-client'
-import { useValidate, useStateValidator } from "react-indicative-hooks"
-import metadata from '../../../../../developer-004/Desktop/metadata.json'
+import { useState, useEffect } from "react"
+import { useStateValidator, useValidate } from "react-indicative-hooks"
+import { useValidateAll } from "react-indicative-hooks"
+import { create } from "ipfs-http-client"
+import { Box, Button, Container, Text, Heading, HStack, Input, Spacer, Image, Spinner, Link, FormControl, FormLabel } from '@chakra-ui/react'
 
+const client = create("https://ipfs.infura.io:5001/api/v0")
 
-const rules = {
-  name: "required",
-  email: "required|email"
+const faves = {
+  anime: '',
+  manga: ''
 }
 
-const messages = {
-  "name.required": "Please, fill the name input using your real name",
-  "email.email": "You need to enter a valid email"
-}
+export default function Home() {
+  const [show, setShow] = useState(false)
+  const [hasChosenImage, setHasChosenImage] = useState(false)
+  const [hasChosenVideo, setHasChosenVideo] = useState(false)
+  const [hasChosenAudio, setHasChosenAudio] = useState(false)
+  const [hasChosenJson, setHasChosenJson] = useState(false)
+  const [fileUrlImage, updateFileUrlImage] = useState('')
+  const [fileUrlVideo, updateFileUrlVideo] = useState('')
+  const [fileUrlAudio, updateFileUrlAudio] = useState('')
+  const [fileUrlJson, updateFileUrlJson] = useState('')
+  const [fileUrl, setFileUrl] = useState('')
+  const [fileData, setFileData] = useState([])
 
-const client = create('https://ipfs.infura.io:5001/api/v0')
+  const data = []
 
-export default function Home({ time }) {
-  const [name, setName] = useState("")
-  const [email, setEmail] = useState("")
-  const error = useValidate({ name, email }, rules, messages)
-  const [fileImage, updateFileImage] = useState(``)
-  const [fileVideo, updateFileVideo] = useState(``)
-  const [fileAudio, updateFileAudio] = useState(``)
-  const [fileData, updateFileData] = useState(``)
+  const [item, setItem] = useState(faves)
+
 
   async function onChangeImage(e) {
+    setHasChosenImage(true)
     const file = e.target.files[0]
     try {
       const added = await client.add(file)
       const url = `https://cloudflare-ipfs.com/ipfs/${added.path}`
-      updateFileImage(url)
-      console.log("IMAGE CID ", added)
-      console.log("IMAGE URL ", url)
+      console.log(added)
+      console.log("cid: " + added.cid)
+      console.log("url: " + url)
+      updateFileUrlImage(url)
+      data.push({
+        "image": added.path
+      })
+      const dataJson = JSON.stringify(data)
+      console.log(data)
+      const blob = new Blob([dataJson], { type: 'application/json' })
+      const jsonFile = new File([ blob ], 'file.json')
+      const otherAdded = await client.add(jsonFile)
+      const otherUrl = `https://cloudflare-ipfs.com/ipfs/${otherAdded.path}`
+      console.log(otherAdded)
+      console.log("cid: " + otherAdded.cid)
+      console.log("url: " + otherUrl)
+      updateFileUrlJson(otherUrl)
     } catch (error) {
       console.log('Error uploading file: ', error)
     }  
   }
+
   async function onChangeVideo(e) {
-    const file = e.target.files[0]
-    try {
-      const added = await client.add(file)
-      const url = `https://ipfs.infura.io/ipfs/${added.path}`
-      updateFileVideo(url)
-      console.log("VIDEO CID ", added)
-      console.log("VIDEO URL ", url)
-    } catch (error) {
-      console.log('Error uploading file: ', error)
-    }  
-  }
-  async function onChangeAudio(e) {
+    setHasChosenVideo(true)
     const file = e.target.files[0]
     try {
       const added = await client.add(file)
       const url = `https://cloudflare-ipfs.com/ipfs/${added.path}`
-      updateFileAudio(url)
-      console.log("AUDIO CID ", added)
-      console.log("AUDIO URL ", url)
+      console.log(added)
+      console.log("cid: " + added.cid)
+      console.log("url: " + url)
+      updateFileUrlVideo(url)
     } catch (error) {
       console.log('Error uploading file: ', error)
     }  
   }
-  async function onChangeData(e) {
+
+  async function onChangeAudio(e) {
+    setHasChosenAudio(true)
     const file = e.target.files[0]
     try {
       const added = await client.add(file)
-      const url = `https://ipfs.infura.io/ipfs/${added.path}`
-      updateFileData(url)
-      console.log("METADATA", added)
-      console.log("METADATA URL ", url)
+      const url = `https://cloudflare-ipfs.com/ipfs/${added.path}`
+      console.log(added)
+      console.log("cid: " + added.cid)
+      console.log("url: " + url)
+      updateFileUrlAudio(url)
     } catch (error) {
       console.log('Error uploading file: ', error)
     }  
   }
-  function revalidate() {
-    fetch('/api/revalidate')
+
+  async function uploadItem() {
+    const jsonData = JSON.stringify(item)
+    console.log(jsonData)
+    try {
+      const added = await client.add(jsonData)
+      const url = `https://cloudflare-ipfs.com/ipfs/${added.path}`
+      console.log(added)
+      console.log("cid: " + added.cid)
+      console.log("url: " + url)
+      updateFileUrlJson(url)
+    } catch (error) {
+      console.log('Error uploading file: ', error)
+    }  
   }
+
+  async function getJsonData(files) {
+    try{
+      const items = await fetch(files)
+      const json = await items.json()
+      setFileData(json)
+    }
+    catch(e) {
+      console.log('Error cannot get files')
+    }
+  }
+
+  function onChange ({ target: { name, value } }) {
+    setItem({ ...item, [name]: value })
+  }
+
+
   return (
-    <Box py={50} bgGradient="linear(to-r, #70e1f5, #ffd194)">
-      <Container>
-        <Stack>
-          <Heading>{time}</Heading>
-          <Button onClick={() => revalidate()}>Revalidate</Button>
-        </Stack>
-        <Stack pt={50}>
-          <Heading>Indicative Hooks</Heading>
-          <Heading size="md">Name</Heading>
-          <Input placeholder="Bryan Diolata" value={name} onChange={e => setName(e.target.value)} />
-          <Heading size="md">Email</Heading>
-          <Input placeholder="bryan@?" value={email} onChange={e => setEmail(e.target.value)} />
-          <Text textShadow="1px 1px" color="red.600">{error && error.message}</Text>
-        </Stack>
-        <Stack pt={50}>
-          <Heading>IPFS Image</Heading>
-          <Input type="file" onChange={onChangeImage} />
-            { fileImage && ( 
-            <Image h="400px" w="100%" objectFit="cover" objectPosition="center" src={fileImage} /> 
-            // <video width="320" height="240" controls>
-            //   <source src={fileUrl} type="video/mp4" />
-            // </video>
-            ) }
-        </Stack>
-        <Stack pt={50}>
-          <Heading>IPFS Video</Heading>
-          <Input type="file" onChange={onChangeVideo} />
-            { fileVideo && ( 
-            <video height="240px" width="800px" controls autoPlay>
-              <source src={fileVideo} type="video/mp4" />
+    <Box h="100vh" bgGradient="linear(to-r, #283c86, #45a247)">
+      <Container maxW="container.sm" pt={50} color="white">
+        <Heading size="xl" mb={4}>IPFS</Heading>
+        <Heading size="md" mb={2}>Image</Heading>
+        <Input
+          type="file"
+          variant="unstyled"
+          onChange={onChangeImage}
+          mb={4} />
+        {
+          (!fileUrlImage && hasChosenImage === true) ? (
+            <Spinner />
+           ) : null
+        }    
+        {
+          fileUrlImage && (
+            <Box>
+              <Image src={fileUrlImage} w="full" h="800px" />
+              <Link as="a" href={fileUrlJson} target="_blank">
+                Go to the link of json metadata
+              </Link>
+            </Box>
+          )
+        }
+        <Heading size="md" mb={2}>Video</Heading>
+        <Input
+          type="file"
+          variant="unstyled"
+          onChange={onChangeVideo}
+          mb={4} />
+        {
+          (!fileUrlVideo && hasChosenVideo === true) ? (
+            <Spinner />
+           ) : null
+        }    
+        {
+          fileUrlVideo && (
+            <video width="320" height="240" controls>
+              <source src={fileUrlVideo} type="video/mp4" />
+              <source src={fileUrlVideo} type="video/mov" />
+              Your browser does not support the video tag.
             </video>
-            ) }
-        </Stack>
-        <Stack pt={50}>
-          <Heading>IPFS Audio</Heading>
-          <Input type="file" onChange={onChangeAudio} />
-            { fileAudio && ( 
-            <Center>
-              <audio width="800px" controls>
-                <source src={fileAudio} type="audio/mpeg" />
-              </audio>
-            </Center>
-            ) }
-        </Stack>
-        <Stack pt={50}>
-          <Heading>IPFS Metadata</Heading>
-            <Text>Upload your JSON file</Text>
-            <Input type="file" onChange={onChangeData} />
-            { fileData && ( 
-            <Center>
-              <Link as="a" href={fileData} target="_blank">Click me</Link>
-            </Center>
-            ) }
-        </Stack>
-        
+          )  
+        }
+        <Heading size="md" mb={2}>Audio</Heading>
+        <Input
+          type="file"
+          variant="unstyled"
+          onChange={onChangeAudio}
+          mb={4} />
+        {
+          (!fileUrlAudio && hasChosenAudio === true) ? (
+            <Spinner />
+           ) : null
+        }  
+        {
+          fileUrlAudio && (
+            <audio width="320" height="240" controls>
+              <source src={fileUrlAudio} type="audio/mp3" />
+              Your browser does not support the video tag.
+            </audio>
+          )
+        }
+        <Heading size="md" mb={2}>JSON</Heading>
+        <FormControl mb={4}>
+          <FormLabel>Favorite Anime</FormLabel>
+          <Input name="anime" variant="filled" onChange={onChange} />
+        </FormControl>
+        <FormControl mb={4}>
+          <FormLabel>Favorite Manga</FormLabel>
+          <Input name="manga" variant="filled" onChange={onChange} />
+        </FormControl>
+        {
+          (!fileUrlJson && hasChosenJson == false) ? 
+          <Button onClick={uploadItem} bg="teal.300">UPLOAD!</Button>
+          : ''
+        }
+        {/* <Input
+          type="file"
+          onChange={onChangeJson}
+          mb={4} /> */}
+        {
+          (!fileUrlJson && hasChosenJson === true) ? (
+            <Spinner />
+           ) : null
+        }  
+        {
+          fileUrlJson && (
+            <Link as="a" href={fileUrlJson} target="_blank">
+              <Button bg="teal.300">Get JSON Link</Button>
+            </Link>
+          )
+        }
+        {/* <Heading size="xl" mt={4} mb={2}>Static Regeneration</Heading>
+        <Text mb={2}>{time}</Text>
+        <Button onClick={() => revalidate()} mb={16}>Revalidate</Button> */}
+        {/* {
+          res.map((result, rKey) => (
+            <Text key={rKey}>{result}</Text>
+          ))
+        } */}
+        <Button bg="teal.500" onClick={() => setShow(true)}>Fetch Data</Button>
+        {
+          show == true ?
+          <Box mt={50}>
+            <Text>bryan</Text>
+            <Button onClick={() => setShow(false)}>Close</Button>
+          </Box>
+          : null
+        }
       </Container>
     </Box>
   )
 }
-export function getStaticProps() {
-  console.log('[Next.js] Running getStaticProps...')
-  return{
-    props: {
-      time: new Date().toISOString(),
-    },
-      revalidate: 60,
-  }
-}
-
